42fce5afcc68d3e7e95bca9ea73dbcacb2bb55ba
kheira Mihoub <66508465+khieramihoub@users.noreply.github.com>
2024-10-07 06:04:11 +0100

    Create bear and big brother.cpp

---
+++ bear and big brother.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define KHEIRA ios_base::sync_with_stdio(false);
+#define WAH cin.tie(NULL);
+const int MOD = 1e9 + 7;
+int main() {
+    KHEIRA WAH
+    int a,b ;
+    cin >> a >> b ; 
+    int y = 0 ; 
+   while( a <= b ){
+    a*= 3 ; 
+    b*=2 ; 
+    y++;
+   }
+    cout << y << endl ;
+    return 0 ; 
+    
+}
+
+ 
+// K     K    H     H   EEEEE    IIIII     RRRRR         A 
+//  K   K      H     H   E          I       R    R      A  A    
+//  K K        H     H   E          I       R    R     A    A   
+//  KKK        HHHHHHH   EEEEE      I       RRRRR     AAAAAAA
+//  K  K       H     H   E          I       R  R     A       A 
+//  K   K      H     H   E          I       R   R   A         A
+//  K    K     H     H   EEEEE    IIIII     R    R A           A
+
+
+
