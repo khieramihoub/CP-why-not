99dc37527c4ddfc1edf520d0b5981d9be3f91f30
kheira Mihoub <66508465+khieramihoub@users.noreply.github.com>
2024-10-18 13:48:50 +0100

    Create Xor-distance.cpp

---
+++ Xor-distance.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#define int long long
+using namespace std;
+#define KHEIRA ios_base::sync_with_stdio(false);
+#define WAH cin.tie(NULL);
+//send help do3t f lycée
+//inshallah matkounch fake sol
+signed main(){
+    KHEIRA WAH
+    int t; cin >> t;
+    while(t--){
+        int a, b, r; cin >> a >> b >> r;
+        int x = 0;
+        bool wah = true;
+        if(a > b) swap(a, b);
+        for(int i = 60; i >= 0; i--){
+            int A = ((a & (1LL << i)) == (1LL << i));
+            int B = ((b & (1LL << i)) == (1LL << i));
+            if(A != B){
+                if(wah){
+                    wah = false;
+                    continue;
+                }
+                if(A == 0 && (x | (1LL << i)) <= r){
+                    x |= (1LL << i);
+                    a ^= (1LL << i);
+                    b ^= (1LL << i);
+                }
+            }
+        }
+        cout << b - a << endl;
+    }
+}
+
+// K     K     H     H   EEEEE    IIIII     RRRRR         A 
+//  K   K      H     H   E          I       R    R      A  A    
+//  K K        H     H   E          I       R    R     A    A   
+//  KKK        HHHHHHH   EEEEE      I       RRRRR     AAAAAAA
+//  K  K       H     H   E          I       R  R     A       A 
+//  K   K      H     H   E          I       R   R   A         A
+//  K    K     H     H   EEEEE    IIIII     R    R A           A
+//⣿⣿⡿⠋⠄⡀⣿⣿⣿⣿⣿⣿⣿⠿⠛⠋⣉⣉⣉⡉⠙⠻
+//⣿⣿⣇⠔⠈⣿⣿⣿⣿⡿⠛⢉⣤⣶⣾⣿⣿⣿⣿⣿⣿⣦
+//⣿⠃⠄⢠⣾⣿⣿⠟⢁⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⡟⠁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⠋⢠⣾⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿
+//⡿⠁⣰⣿⣿⣿⣿⣿⣿⣿⣿⠗⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿
+//⠁⣼⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⣠⣄⢰⣿⣿⣿⣿⣿⣿
+//⣼⣿⣿⣿⣿⣿⣿⡇⠄⢀⡴⠚⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⢰⣿⣿⣿⣿⣿⡿⣿⣿⠴⠋⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⡟
+//⣿⣿⣿⣿⣿⣿⠃⠈⠁⠄⠄⢀⣴⣿⣿⣿⣿⣿⣿⣿⡟⢀
+//⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⢶⣿⣿⣿⣿⣿⣿⣿⣿⠏⢀⣾
+//⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⠋⣠⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣼⣿⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿
+//⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢁⣴⣿⣿⣿⠗⠄⠄⣿
+//⠈⠻⣿⣿⣿⣿⣿⣿⠿⠛⣉⣤⣾⣿⣿⣿⣿⣇⠠⠺⣷⣿
+//⣦⣄⣈⣉⣉⣉⣡⣤⣶⣿⣿⣿⣿⣿⣿⣿⠉⠁⣀⣼⣿⣿
+
+
+
