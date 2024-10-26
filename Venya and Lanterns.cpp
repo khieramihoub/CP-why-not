4620186fb73ad1b03b8bb581004c217d18c6dab1
kheira Mihoub <66508465+khieramihoub@users.noreply.github.com>
2024-10-26 15:32:07 +0100

    Create Venya and Lanterns.cpp

---
+++ Venya and Lanterns.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define KHEIRA ios_base::sync_with_stdio(false);
+#define WAH cin.tie(NULL);
+
+int main() {
+    KHEIRA WAH
+    int n , l; 
+    cin >> n >> l;
+    vector<int> fanous(n) ; 
+    for(int i = 0 ; i < n ; ++i){
+        cin >> fanous[i] ; 
+    }
+    sort(fanous.begin(), fanous.end()) ; 
+    double max_gap = 0.0; 
+    for(int i = 1 ; i < n ; ++i){
+        max_gap = max(max_gap , static_cast<double>(fanous[i]-fanous[i-1]));
+    }
+    double dist_to_start = fanous[0]-0 ;
+    double dist_to_end = l- fanous[n-1] ;
+    double wah = max(max_gap / 2.0 ,max(dist_to_start , dist_to_end)) ; 
+    cout << fixed << setprecision(10) << wah << endl ;
+
+    
+    return 0;
+}
+
+
+// K     K     H     H   EEEEE    IIIII     RRRRR         A 
+//  K   K      H     H   E          I       R    R      A  A    
+//  K K        H     H   E          I       R    R     A    A   
+//  KKK        HHHHHHH   EEEEE      I       RRRRR     AAAAAAA
+//  K  K       H     H   E          I       R  R     A       A 
+//  K   K      H     H   E          I       R   R   A         A
+//  K    K     H     H   EEEEE    IIIII     R    R A           A
+//⣿⣿⡿⠋⠄⡀⣿⣿⣿⣿⣿⣿⣿⠿⠛⠋⣉⣉⣉⡉⠙⠻
+//⣿⣿⣇⠔⠈⣿⣿⣿⣿⡿⠛⢉⣤⣶⣾⣿⣿⣿⣿⣿⣿⣦
+//⣿⠃⠄⢠⣾⣿⣿⠟⢁⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⡟⠁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⠋⢠⣾⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿
+//⡿⠁⣰⣿⣿⣿⣿⣿⣿⣿⣿⠗⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿
+//⠁⣼⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⣠⣄⢰⣿⣿⣿⣿⣿⣿
+//⣼⣿⣿⣿⣿⣿⣿⡇⠄⢀⡴⠚⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⢰⣿⣿⣿⣿⣿⡿⣿⣿⠴⠋⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⡟
+//⣿⣿⣿⣿⣿⣿⠃⠈⠁⠄⠄⢀⣴⣿⣿⣿⣿⣿⣿⣿⡟⢀
+//⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⢶⣿⣿⣿⣿⣿⣿⣿⣿⠏⢀⣾
+//⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⠋⣠⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣼⣿⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿
+//⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢁⣴⣿⣿⣿⠗⠄⠄⣿
+//⠈⠻⣿⣿⣿⣿⣿⣿⠿⠛⣉⣤⣾⣿⣿⣿⣿⣇⠠⠺⣷⣿
+//⣦⣄⣈⣉⣉⣉⣡⣤⣶⣿⣿⣿⣿⣿⣿⣿⠉⠁⣀⣼⣿⣿
