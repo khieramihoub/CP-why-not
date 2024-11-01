5def83e4507a0184cb46e6ab850b2909677c1453
kheira Mihoub <66508465+khieramihoub@users.noreply.github.com>
2024-11-01 11:59:10 +0100

    Create array_division.cpp

---
+++ array_division.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define KHEIRA ios_base::sync_with_stdio(false);
+#define WAH cin.tie(NULL);
+bool divide(const vector<int>& a , int k , long long maxsomme){
+   int sub_a = 1 ; long long current_somme = 0 ; 
+   for(int num : a){
+      if(current_somme + num > maxsomme){
+         ++sub_a ; 
+         current_somme = num; 
+      if(sub_a > k ) return false ; 
+      }else{
+         current_somme += num;
+      }
+   }
+   return true ; 
+}
+
+
+int main() {
+    KHEIRA WAH
+    int n , k ; cin >> n >> k ; 
+    vector<int> a(n); 
+    for(int i = 0 ;  i < n ; ++i){
+      cin >> a[i] ; 
+    }
+    long long left = *max_element(a.begin(),a.end()) ; 
+    long long right = accumulate(a.begin() , a.end() , 0LL) ; 
+    long long res = right ; 
+    while(left <= right){
+      long long mid = left + (right - left) / 2 ; 
+      if(divide(a , k , mid)){
+         res = mid ; 
+         right = mid - 1 ;
+    }else{
+      left = mid+1 ; 
+    }
+    }
+    cout << res << endl ; 
+    return 0;
+}
+
+
+// K     K     H     H   EEEEE    IIIII     RRRRR         A 
+//  K   K      H     H   E          I       R    R      A  A    
+//  K K        H     H   E          I       R    R     A    A   
+//  KKK        HHHHHHH   EEEEE      I       RRRRR     AAAAAAA
+//  K  K       H     H   E          I       R  R     A       A 
+//  K   K      H     H   E          I       R   R   A         A
+//  K    K     H     H   EEEEE    IIIII     R    R A           A
+//⣿⣿⡿⠋⠄⡀⣿⣿⣿⣿⣿⣿⣿⠿⠛⠋⣉⣉⣉⡉⠙⠻
+//⣿⣿⣇⠔⠈⣿⣿⣿⣿⡿⠛⢉⣤⣶⣾⣿⣿⣿⣿⣿⣿⣦
+//⣿⠃⠄⢠⣾⣿⣿⠟⢁⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⡟⠁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⠋⢠⣾⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿
+//⡿⠁⣰⣿⣿⣿⣿⣿⣿⣿⣿⠗⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿
+//⠁⣼⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⣠⣄⢰⣿⣿⣿⣿⣿⣿
+//⣼⣿⣿⣿⣿⣿⣿⡇⠄⢀⡴⠚⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⢰⣿⣿⣿⣿⣿⡿⣿⣿⠴⠋⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⡟
+//⣿⣿⣿⣿⣿⣿⠃⠈⠁⠄⠄⢀⣴⣿⣿⣿⣿⣿⣿⣿⡟⢀
+//⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⢶⣿⣿⣿⣿⣿⣿⣿⣿⠏⢀⣾
+//⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⠋⣠⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣼⣿⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿
+//⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢁⣴⣿⣿⣿⠗⠄⠄⣿
+//⠈⠻⣿⣿⣿⣿⣿⣿⠿⠛⣉⣤⣾⣿⣿⣿⣿⣇⠠⠺⣷⣿
+//⣦⣄⣈⣉⣉⣉⣡⣤⣶⣿⣿⣿⣿⣿⣿⣿⠉⠁⣀⣼⣿⣿
+
+
+
