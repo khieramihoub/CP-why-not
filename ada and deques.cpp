c8f4287cf6d0ff6b24dfcb916b23fc140aba236d
kheira Mihoub <66508465+khieramihoub@users.noreply.github.com>
2024-12-14 21:39:32 +0100

    Create ada and deques.cpp

---
+++ ada and deques.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define KHEIRA ios_base::sync_with_stdio(false);
+#define WAH cin.tie(NULL);
+//wa3333333333333
+int main() {
+    KHEIRA WAH 
+    int q;
+    cin >> q;
+    deque<int> wah;
+    bool isReversed = false; 
+    string output; 
+
+    while (q--) {
+        string s;
+        cin >> s;
+
+        if (s == "toFront") {
+            int n;
+            cin >> n;
+            if (isReversed) {
+                wah.push_back(n); 
+            } else {
+                wah.push_front(n);
+            }
+        } else if (s == "push_back") {
+            int n;
+            cin >> n;
+            if (isReversed) {
+                wah.push_front(n); 
+            } else {
+                wah.push_back(n); 
+            }
+        } else if (s == "front") {
+            if (wah.empty()) {
+                output += "No job for Ada?\n";
+            } else {
+                if (isReversed) {
+                    output += to_string(wah.back()) + "\n";
+                    wah.pop_back();
+                } else {
+                    output += to_string(wah.front()) + "\n";
+                    wah.pop_front();
+                }
+            }
+        } else if (s == "back") {
+            if (wah.empty()) {
+                output += "No job for Ada?\n";
+            } else {
+                if (isReversed) {
+                    output += to_string(wah.front()) + "\n";
+                    wah.pop_front();
+                } else {
+                    output += to_string(wah.back()) + "\n";
+                    wah.pop_back();
+                }
+            }
+        } else if (s == "reverse") {
+            isReversed = !isReversed; 
+        }
+    }
+    cout << output;
+
+    return 0;
+}
+
+// K     K     H     H   EEEEE    IIIII     RRRRR         A 
+//  K   K      H     H   E          I       R    R      A  A    
+//  K K        H     H   E          I       R    R     A    A   
+//  KKK        HHHHHHH   EEEEE      I       RRRRR     AAAAAAA
+//  K  K       H     H   E          I       R  R     A       A 
+//  K   K      H     H   E          I       R   R   A         A
+//  K    K     H     H   EEEEE    IIIII     R    R A           A
+//⣿⣿⡿⠋⠄⡀⣿⣿⣿⣿⣿⣿⣿⠿⠛⠋⣉⣉⣉⡉⠙⠻
+//⣿⣿⣇⠔⠈⣿⣿⣿⣿⡿⠛⢉⣤⣶⣾⣿⣿⣿⣿⣿⣿⣦
+//⣿⠃⠄⢠⣾⣿⣿⠟⢁⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⡟⠁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⠋⢠⣾⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿
+//⡿⠁⣰⣿⣿⣿⣿⣿⣿⣿⣿⠗⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿
+//⠁⣼⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⣠⣄⢰⣿⣿⣿⣿⣿⣿
+//⣼⣿⣿⣿⣿⣿⣿⡇⠄⢀⡴⠚⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⢰⣿⣿⣿⣿⣿⡿⣿⣿⠴⠋⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⡟
+//⣿⣿⣿⣿⣿⣿⠃⠈⠁⠄⠄⢀⣴⣿⣿⣿⣿⣿⣿⣿⡟⢀
+//⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⢶⣿⣿⣿⣿⣿⣿⣿⣿⠏⢀⣾
+//⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⠋⣠⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣼⣿⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿
+//⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢁⣴⣿⣿⣿⠗⠄⠄⣿
+//⠈⠻⣿⣿⣿⣿⣿⣿⠿⠛⣉⣤⣾⣿⣿⣿⣿⣇⠠⠺⣷⣿
+//⣦⣄⣈⣉⣉⣉⣡⣤⣶⣿⣿⣿⣿⣿⣿⣿⠉⠁⣀⣼⣿⣿
