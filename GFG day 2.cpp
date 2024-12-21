6e28a47f207c300c4bfd6be661b019ba306ff3c9
kheira Mihoub <66508465+khieramihoub@users.noreply.github.com>
2024-12-21 07:52:12 +0100

    Create GFG day 2.cpp

---
+++ GFG day 2.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+       int n = arr.size() ; 
+       vector<int> wah(n); 
+       int j = 0 ; 
+       for(int i = 0 ; i < n ; ++i){
+          if(arr[i] != 0){
+             wah[j++] = arr[i] ;  
+          }
+       }
+       while(j<n){
+           wah[j++] = 0 ; 
+       }
+       for(int i = 0 ; i < n ; ++i){
+           arr[i] = wah[i] ;  
+       }
+
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int n = arr.size();
+        ob.pushZerosToEnd(arr);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
