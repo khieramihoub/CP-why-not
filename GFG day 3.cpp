fec7c057a40bde7ac64ca067d25a71d1213aa5a3
kheira Mihoub <66508465+khieramihoub@users.noreply.github.com>
2024-12-21 12:45:36 +0100

    Create GFG day 3.cpp

---
+++ GFG day 3.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        int n = arr.size() ; 
+        vector<int> wah(n) ; 
+        for(int i = 0 ; i < n ; ++i){
+            wah[i] = arr[n-i-1] ; 
+        }
+        for(int i = 0 ; i < n ; ++i){
+            arr[i] = wah[i] ; 
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.reverseArray(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
