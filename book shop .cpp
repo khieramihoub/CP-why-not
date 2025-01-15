71f8d99f904f34090919326cf4ffbf00cd65948b
kheira Mihoub <66508465+khieramihoub@users.noreply.github.com>
2025-01-15 22:05:41 +0100

    Create book shop .cpp

---
+++ book shop .cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define COUZINA ios::sync_with_stdio(false);
+#define WAH cin.tie(nullptr);
+ 
+int main(){ 
+    COUZINA WAH  
+    int n , x ; cin >> n >> x ; 
+    vector<int> price(n); 
+    vector<int> pages(n);
+    for(int i = 0 ; i < n ; ++i){
+        cin >> price[i] ; 
+    }
+    for(int i = 0 ; i < n ; ++i){
+        cin >> pages[i] ; 
+    }
+    vector<vector<int>> dp(n+1 , vector<int>(x+1,0)) ; 
+    for(int i = 1 ; i <= n ; ++i){
+        for(int j = x ; j >= 0 ; --j){
+            int wah = price[i-1] ; 
+            int ouais = pages[i-1] ; 
+            int eddi = (j >= wah ? dp[i-1][j - wah] + ouais : 0) ; 
+            int khelli = dp[i-1][j] ; 
+            dp[i][j] = max(eddi , khelli) ; 
+        }
+    }
+cout << dp[n][x] << endl ; 
+ 
+    return 0;
+}
+ 
+//  ░▒▓██████▓▒░   ░▒▓██████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓████████▓▒░ ░▒▓█▓▒░ ░▒▓███████▓▒░   ░▒▓██████▓▒░  
+// ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░        ░▒▓█▓▒░ ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ 
+// ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓██▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ 
+// ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░    ░▒▓██▓▒░    ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓████████▓▒░ 
+// ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓██▓▒░      ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ 
+// ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ 
+//  ░▒▓██████▓▒░   ░▒▓██████▓▒░   ░▒▓██████▓▒░  ░▒▓████████▓▒░ ░▒▓█▓▒░ ░▒▓█▓▒░
