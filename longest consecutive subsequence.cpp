d34ea9971d7532af00d321f02dfa75166917f90f
kheira Mihoub <66508465+khieramihoub@users.noreply.github.com>
2025-02-14 08:14:58 +0100

    Update longest consecutive subsequence.cpp

--- longest consecutive subsequence.cpp
+++ longest consecutive subsequence.cpp
@@ -18,4 +18,23 @@ public:
         }
         return max_length ; 
     }
+}; /// tlee lmaoo 
+//this code is correct 
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.empty()) return 0 ;
+        sort(nums.begin() , nums.end()) ; 
+        int max_length = 1 , current_length = 1 ; 
+        for(int i = 1 ; i < nums.size() ; ++i){
+            if(nums[i] == nums[i-1]) continue ; 
+            if(nums[i] == nums[i-1] + 1){
+                current_length++ ; 
+            }else{
+                max_length = max(max_length, current_length) ; 
+                current_length = 1 ; 
+            }
+        }
+    return max(max_length,current_length) ; 
+    }
 };
