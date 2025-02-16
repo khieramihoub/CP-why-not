0ac80c447621492c7c75d8ad37f8cc19b27738be
kheira Mihoub <66508465+khieramihoub@users.noreply.github.com>
2025-02-16 22:16:58 +0100

    Create rdwanprblm.cpp

---
+++ rdwanprblm.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define KHEIRA ios::sync_with_stdio(false);
+#define WAH cin.tie(nullptr);
+int main() {
+   KHEIRA WAH
+   int wah , hawcha ; cin >> wah >> hawcha ; 
+   cout << "red" << wah/hawcha << endl ; 
+   return 0 ; 
+   }
